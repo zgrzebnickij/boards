c805dd5458fb58e76331fba4f7d3aebff7ff1508
Jakub Zgrzebnicki <jakub.zgrzeb@gmail.com>
2018-09-24 16:57:32 +0200

    adding forms
    thats awsome

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
-
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
 ]
--- views.py
+++ views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Board
-
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Board, User,Topic,Post
+from .forms import NewTopicForm
 # Create your views here.
 def home(request):
     boards = Board.objects.all()
     return render(request,'home.html',{'boards':boards})
+
+def board_topics(request,pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
